30cbac8117d8a1a3751db2b42fbd995ebebee619
hsieh jing <xqj198404@gmail.com>
2018-01-11 18:42:46 +0800

    增加项目管理服务实现项目

---
+++ Hayaa.ServiceFrame/Hayaa.ProjectService.Core/BussinessProjectServer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Hayaa.BaseModel;
+using Hayaa.ServiceFrame.Model.Project;
+
+namespace Hayaa.ProjectService.Core
+{
+    public class BussinessProjectServer : BussinessProjectService
+    {
+        public FunctionResult<Project> Create(Project info)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BaseFunctionResult DeleteByID(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public GridPager<Project> GetPager(GridPagerPamater<ProjectGridSearch> searchParam)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BaseFunctionResult UpdateByID(Project info)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
