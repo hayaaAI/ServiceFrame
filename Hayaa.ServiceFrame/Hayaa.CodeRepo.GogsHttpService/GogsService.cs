a87f12ddbfe17418ca8c18cd60ce4a42052692e2
hsieh jing <xqj198404@gmail.com>
2018-01-16 19:02:27 +0800

    增加gogs组件

---
+++ Hayaa.ServiceFrame/Hayaa.CodeRepo.GogsHttpService/GogsService.cs
@@ -0,0 +1,10 @@
+﻿using Hayaa.BaseModel;
+using System;
+
+namespace Hayaa.CodeRepo.GogsHttpService
+{
+    public interface GogsService
+    {
+        FunctionOpenResult<String> CreateGitRepo();
+    }
+}
